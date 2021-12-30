aa1bd9ece6b1731e808c165f02a8a11df538c2b4
Minseon Kim <johndoe@example.com>
2021-12-30 20:59:33 +0900

    처음 저장소에 올립

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const productSchema = mongoose.Schema({
+	name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5,
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+}, { timestamps: true })
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = {User}
+
+module.exports = { Product }
