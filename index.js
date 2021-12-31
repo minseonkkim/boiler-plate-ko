81ad06b3d98c840b8ed1c5173a78080bfa043428
Minseon Kim <johndoe@example.com>
2021-12-31 11:35:19 +0900

    비밀 설정

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 4000;
 const bodyParser = require('body-parser');
 const { User } = require('./models/User');
+const config = require('./config/key');
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://minseon:jewely2001@cluster0.ylfdo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI, {})
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
